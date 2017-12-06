1b6b645daf7770ad195f5ce410c3b78deabe8fdd
osamab1 <34302883+osamab1@users.noreply.github.com>
2017-12-06 14:27:08 +0100

    Update prog.c

--- prog.c
+++ prog.c
@@ -1,4 +1,10 @@
 int main (int argc, char** argv){
 
-  return 0;
+	char **messages = NULL;
+	*messages = malloc(5*sizeof(char*));
+	for(int i = 0; i < 5; i++){
+		messages[i] = malloc (10*sizeof(char));
+	}
+
+	return 0;
 }
